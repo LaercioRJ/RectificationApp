34d5ba9fc0c89842764ee42f34fb5d06dc9d5a95
unknown <laerciorjunior@gmail.com>
2020-12-18 23:47:19 -0300

    Top bar

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-topbar',
+  templateUrl: './top-bar.component.html',
+  styleUrls: ['./top-bar.component.css']
 })
 export class AppComponent {
   title = 'rectification-app';
